c9bf19acae88cdf74de92b934a5ed472bb1ec0c8
nasols <nasbrigtols@gmail.com>
2021-08-21 13:37:38 +0200

    Create pygame_lok.py

---
+++ main/pygame/pygame_lok.py
@@ -0,0 +1,22 @@
+
+import pygame
+
+##image = pygame.image.load('01_image.jpg')
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((500,500))
+
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+
+if __name__=="__main__":
+    main()
