82b31835e3c47cccb4313be21411947f1bf9688d
hennabenna11 <loghenrik@gmail.com>
2021-08-22 21:02:57 +0200

    ent class

--- main/game/entity.py
+++ main/game/entity.py
@@ -1,9 +1,15 @@
+import pygame
+
 class Entity:
-    def __init__(self):
-        self.width = 10
-        self.height = 10
+    def __init__(self, pos, path):
+        self.pos = pos
+        self.path = "main/game/media/" + path
+        self.img = pygame.image.load(self.path)
+        self.rect = self.img.get_rect()
 
     def say_something(self):
         print("HELLO")
+
+        
 
 
--- main/game/main.py
+++ main/game/main.py
@@ -4,27 +4,38 @@ from entity import Entity
 mainClock = pygame.time.Clock()
 pygame.init()
 
+# Class? + update monitor on resize
 monitor = width, height = 720, 480
-speed = [2, 2]
-black = 0, 0, 0
-
+speed = [0, 0]
+white = 255, 255, 255
+vel = 2
+running = True
+player = Entity([width / 2, height / 2], "intro_ball.gif")
 screen = pygame.display.set_mode(monitor, pygame.RESIZABLE)
 
-ball = pygame.image.load("main/game/intro_ball.gif")
-ballrect = ball.get_rect()
+pygame.display.set_caption("Pukman")
 
-while 1:
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        player.pos[0] -= vel
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    if keys[pygame.K_RIGHT]:
+        player.pos[0] += vel
+
+    if keys[pygame.K_UP]:
+        player.pos[1] -= vel
+
+    if keys[pygame.K_DOWN]:
+        player.pos[1] += vel
+
+    screen.fill(white)
+    screen.blit(player.img, player.pos)
     pygame.display.flip()
     mainClock.tick(60)
 
