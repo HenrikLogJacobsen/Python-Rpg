48d5748f420e5b6aa170559645a735347dcab1ff
nasols <nasbrigtols@gmail.com>
2021-08-24 13:53:40 +0200

    Update test.py

--- main/test.py
+++ main/test.py
@@ -4,10 +4,19 @@ i = 1
 i = 2 
 
 for i in range(5):
+<<<<<<< HEAD
     print('bæsj')   
 
 
 
+=======
+    print('bæsj')
+<<<<<<< HEAD
+    
+    print(1)
+=======
+>>>>>>> f9764bcb6e0f7fe55dfc81999767bdd2b7c3199f
 
 
     
+>>>>>>> c2f8b17192ad816e643d6dd3dafda8ff786f2ee0
