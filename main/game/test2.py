5d580d956430d595646b8ef6680dd84ae9cfce1d
nasols <nasbrigtols@gmail.com>
2021-08-25 17:50:35 +0200

    Update test2.py

--- main/game/test2.py
+++ main/game/test2.py
@@ -1,34 +1,42 @@
+
 import pygame 
 import sys
 import buttons
+import entity
 from pygame.constants import QUIT
 
 pygame.init()
+speed = [0,0]
+vel = 2
 
+LAUNCH_GAME = False
 SCREEN_HEIGHT = 500
 SCREEN_WIDHT = 800
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDHT))
 screen.fill((202, 228, 241))
-start_img = pygame.image.load('/Users/jonasolsen/Documents/GitHub/start.png').convert_alpha()
+start_img = pygame.image.load('/Users/jonasolsen/Documents/GitHub/jonasLog/main/start.png').convert_alpha()
 
 start_button = buttons.Button(100, 200, start_img, 0.7)
+player = entity.Entity([0, 0], "intro_ball.gif")
+player.center_coord(SCREEN_WIDHT / 2, SCREEN_HEIGHT / 2)
 
 
+run = True
 
-run = True 
 while run:
     
     screen.fill((202, 228, 241))
 
     #draw_button = buttons.Button.draw(screen)
-    if start_button.draw(screen) == True:
-        print('kuk')
-
+    start_button.draw(screen)
+    
 
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
             pygame.quit
+        
+    
     
     pygame.display.update()
 
