2b5b634f3d084a25e94a3e9d06a4aacafaf7e0a6
nasols <nasbrigtols@gmail.com>
2021-08-26 20:16:24 +0200

    idk

--- main/game/test2.py
+++ main/game/test2.py
@@ -4,9 +4,12 @@ import sys
 from buttons import Button
 import entity
 from pygame.constants import QUIT
+from map import Map
+
 
 pygame.init()
-speed = [0,0]
+screen_pos = [0, 0]
+white = 255, 255, 255
 vel = 2
 
 
@@ -14,18 +17,33 @@ SCREEN_HEIGHT = 500
 SCREEN_WIDHT = 800
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDHT))
 screen.fill((202, 228, 241))
-start_img = pygame.image.load('main/start.png').convert_alpha()
+start_img = pygame.image.load('main/game/media/start.png').convert_alpha()
+settings_img = pygame.image.load('main/game/media/settings.png').convert_alpha()
+#back_img = pygame.image.load('main/game/media/back.png').convert_alpha()
 
-start_button = Button(100, 200, start_img, 0.6)
+tree_pos = [[-200, 200], [300, 200], [-100, -100], [200, -100]]
+map1 = Map("tree.jpg", tree_pos, screen)
+
+start_button = Button(50, 200, start_img, 0.6)
+settings_button = Button(250, 200, settings_img, 0.6)
+#back_button = Button(250, 400, back_img, 0.2)
+
+
 player = entity.Entity([0, 0], "intro_ball.gif")
 player.center_coord(SCREEN_WIDHT / 2, SCREEN_HEIGHT / 2)
 
+clock = pygame.time.Clock()
+fps = 60
 
-run_start = True
+
+
+run_menu = True
+settings_menu = False
 
 LAUNCH_GAME = False
 
-while run_start:
+
+while run_menu:
 
     
     screen.fill((202, 228, 241))
@@ -33,26 +51,70 @@ while run_start:
     #draw_button = buttons.Button.draw(screen)
     if start_button.draw(screen) == True:
         LAUNCH_GAME = True
-        run_start = False
+        run_menu = False
+    
 
+    if settings_button.draw(screen) == True:
+        settings_menu = True
+        run_menu = False
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            run_start = False
+            run_menu = False
             pygame.quit
         
     pygame.display.update()
+
+
+while settings_menu == True:
+    
+    screen.fill((101,101,101))
+
+    if True:
+        run_menu = True
+        break
+        
+
+    if event.type == QUIT:
+        settings_menu = False
+        pygame.quit
+
+        
+    pygame.display.update()
+    clock.tick(fps)
+
+
 
 while LAUNCH_GAME: 
 
     screen.fill((202, 228, 241))
 
+
     for event in pygame.event.get():
+
         if event.type == QUIT:
-            run_start = False
+            LAUNCH_GAME = False
             pygame.quit
-        
+            
+
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        screen_pos[0] += vel
+    if keys[pygame.K_RIGHT]:
+        screen_pos[0] -= vel
+    if keys[pygame.K_UP]:
+        screen_pos[1] += vel
+    if keys[pygame.K_DOWN]:
+        screen_pos[1] -= vel
+
+    # DRAW
+    screen.blit(player.img, player.pos)
+    map1.draw(screen_pos)
     pygame.display.update()
+    clock.tick(60)
+
+
 
         
     
