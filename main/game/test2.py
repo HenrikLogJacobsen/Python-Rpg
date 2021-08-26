81dfc8e6ee60b5ebdff799d7464a92b3c59adcb3
nasols <nasbrigtols@gmail.com>
2021-08-26 15:19:59 +0200

    Update test2.py

--- main/game/test2.py
+++ main/game/test2.py
@@ -16,7 +16,7 @@ screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDHT))
 screen.fill((202, 228, 241))
 start_img = pygame.image.load('main/start.png').convert_alpha()
 
-start_button = Button(100, 200, start_img, 6)
+start_button = Button(100, 200, start_img, 0.6)
 player = entity.Entity([0, 0], "intro_ball.gif")
 player.center_coord(SCREEN_WIDHT / 2, SCREEN_HEIGHT / 2)
 
@@ -33,17 +33,27 @@ while run_start:
     #draw_button = buttons.Button.draw(screen)
     if start_button.draw(screen) == True:
         LAUNCH_GAME = True
+        run_start = False
 
-
-
-
-
-    
 
     for event in pygame.event.get():
         if event.type == QUIT:
             run_start = False
             pygame.quit
+        
+    pygame.display.update()
+
+while LAUNCH_GAME: 
+
+    screen.fill((202, 228, 241))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run_start = False
+            pygame.quit
+        
+    pygame.display.update()
+
         
     
     
