ffcfb100dbe92cb92f2fcb17ca8c44c4a01d9bee
hennabenna11 <loghenrik@gmail.com>
2021-08-30 23:03:34 +0200

    1

--- main/game/map.py
+++ main/game/map.py
@@ -2,9 +2,9 @@ from entity import Entity
 import pygame 
 
 class Map:
-    def __init__(self, url, positions, screen):
-        self.url = url
-        self.path = 'main/game/media' + url
+    def __init__(self, path, positions, screen):
+        self.path = path
+        self.path = 'main/game/media' + path
         self.positions = positions
         self.screen = screen
         self.entities = []
@@ -12,13 +12,11 @@ class Map:
      
 
         for pos in self.positions:
-            self.entities.append(Entity(pos, self.url, .2))
+            self.entities.append(Entity(pos, self.path, .2))
         
 
-    def draw(self, screen_pos):
+    def draw(self, playerpos):
         for e in self.entities:
-            e.pos[0] = e.start_pos[0] + screen_pos[0]
-            e.pos[1] = e.start_pos[1] + screen_pos[1]
-            self.screen.blit(e.img,e.pos)
-
-
+            e.pos[0] = e.start_pos[0] + playerpos[0]
+            e.pos[1] = e.start_pos[1] + playerpos[1]
+            self.screen.blit(e.img, e.pos)
