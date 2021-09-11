8993a8ecb56edac2218c7c4dd11c2e5a6fb38169
nasols <nasbrigtols@gmail.com>
2021-09-11 02:13:37 +0200

    kuk

--- main/game/drawScreen.py
+++ main/game/drawScreen.py
@@ -1,17 +1,9 @@
 
 import pygame
 
-class redrawGameWindow(self, screen,
-
-    self.screen.fill('white')
-    self.map1.draw(self.screen_pos)
-
-
-    if LEFT:
-        screen.blit(player_left_walk[clock_count],(player.pos))
-
-    elif RIGHT:
-        screen.blit(player_right_walk[clock_count],(player.pos))
-    
-    else: 
-        screen.blit(player_idle[idle_count],(player.pos))
+def redrawGameWindow():
+    screen.fill('white')
+    enemy_pos = enemy.move_towards_player(player.pos)
+    screen.blit(enemy.img, enemy_pos)
+    player.draw(screen)
+    map1.draw(player.pos)
--- main/game/main.py
+++ main/game/main.py
@@ -19,19 +19,21 @@ pygame.display.set_caption("Pukman")
 
 #Entities (trenger lettere måte for når vi får mange)
 player = Player(screen_pos)
-
+step_count = [0, 0, 0, 0]
 enemy = Scellyenny((200,200), 'kuk2.png', 1, 2, screen_pos)
+
 
 map1 = MapInfo("map1", screen)
 
 def redrawGameWindow():
     screen.fill('white')
-    enemy_pos = enemy.move_towards_player(player.pos)
+    enemy_pos = enemy.move_towards_player(step_count)
     screen.blit(enemy.img, enemy_pos)
     player.draw(screen)
     map1.draw(player.pos)
 
-    
+
+
 
 #GAMELOOP
 while running:
@@ -43,18 +45,22 @@ while running:
     if keys[pygame.K_LEFT]:
         player.pos[0] += vel
         player.action = "left"
-
+        step_count[3] -= 1
+        
     if keys[pygame.K_RIGHT]:
         player.pos[0] -= vel
         player.action = "right"
+        step_count[2] += 1
 
     if keys[pygame.K_UP]:
         player.pos[1] += vel
         player.action = "idle"
+        step_count[0] += 1
 
     if keys[pygame.K_DOWN]:
         player.pos[1] -= vel
         player.action = "idle"
+        step_count[1] -= 1
     
     # ----- Må oppdateres -----
     #if keys[pygame.K_SPACE]:
@@ -64,8 +70,8 @@ while running:
     mainClock.tick(60)
     redrawGameWindow()
     player.update()
-    
     pygame.display.flip()
+
         
 
 
